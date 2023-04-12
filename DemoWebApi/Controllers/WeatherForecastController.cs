2d0a3e3035c56b4ca7fc18ce9bef1461e23c3241
Lavit Hotchandani <lavit.hotchandani@jci.com>
2023-04-12 15:18:37 +0530

    second commit

--- DemoWebApi/Controllers/WeatherForecastController.cs
+++ DemoWebApi/Controllers/WeatherForecastController.cs
@@ -29,5 +29,12 @@ namespace DemoWebApi.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        [Route("GetMsg")]
+        public string GetMsg()
+        {
+            return "Hello from get messages action method.";
+        }
     }
 }
